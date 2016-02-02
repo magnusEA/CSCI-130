0f6917d46add959df0e77c7678207d1c8aa6b57e
magnusEA <abundiz120@mail.fresnostate.edu>
2016-02-02 15:53:35 -0800

    Create even.go

---
+++ Challenges/05-Even-numbers/even.go
@@ -0,0 +1,19 @@
+
+package main
+
+import "fmt"
+
+func main() {
+	
+	fmt.Println("---Here you go---")
+	
+	for i := 0; i <= 100; i++ {
+		
+		if i%2 == 0 {
+
+			fmt.Println(i)
+		}
+	}
+
+	fmt.Println("---The End---")
+}
