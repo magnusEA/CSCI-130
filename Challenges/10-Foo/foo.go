76fd2f7a0f2d71f869aef01a1b5df94d42320760
magnusEA <abundiz120@mail.fresnostate.edu>
2016-02-02 16:54:25 -0800

    Create foo.go

---
+++ Challenges/10-Foo/foo.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func foo(num ...int) {
+	fmt.Println(num)
+}
+func main() {
+	foo(1, 2)
+
+	foo(1, 2, 3)
+
+	aSlice := []int{1, 2, 3, 4}
+
+	foo(aSlice...)
+	
+	foo()
+}
