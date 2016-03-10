718ad5a353427f4837f091e86a474ceae2b5edfe
magnusEA <abundiz120@mail.fresnostate.edu>
2016-03-10 13:53:28 -0800

    Create main.go

---
+++ Project/Step1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("edit.html")
+	
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		err = tpl.Execute(res, nil)
+		
+	})
+	http.ListenAndServe(":8080", nil)
+}
