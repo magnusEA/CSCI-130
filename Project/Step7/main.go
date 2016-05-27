7d998c020ffbe2ccf5683a75f72c38d70ae4fba5
magnusEA <abundiz120@mail.fresnostate.edu>
2016-05-27 13:50:24 -0700

    Update main.go

--- Project/Step7/main.go
+++ Project/Step7/main.go
@@ -1,16 +1,64 @@
 package main
 
 import (
-	"io"
+
+//"html/template"
+	"io/ioutil"
 	"net/http"
+	"io"
+	"html/template"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"github.com/nu7hatch/gouuid"
 )
 
-func main() {
-	http.HandleFunc("/", login)
-	http.ListenAndServe(":8080", nil)
+type User struct {
+	Uuid, Name, Age, Hmac string
 }
 
-func login(res http.ResponseWriter, req *http.Request) {
+
+func main() {
+	http.HandleFunc("/", serveLogin)
+	http.ListenAndServe(":8080", nil)
+}
+var loginFile string
+
+func init() {
+	temp, _ := ioutil.ReadFile("user_templates/temp2.html")
+	loginFile = string(temp)
+}
+
+func getHMAC(data string) string {
+	hmac_code := hmac.New(sha256.New, []byte(data+"plebKey"))
+	return string(hmac_code.Sum(nil))
+}
+func bakeUserCookie(cookie *http.Cookie, req *http.Request) string {
+	jsonVal, _ := undoJSON(cookie)
+	jsonVal.Name = req.FormValue("name")
+	jsonVal.Age = req.FormValue("age")
+	jsonVal.Hmac = req.FormValue("HMAC")
+	return redoJSON(jsonVal)
+}
+
+func redoJSON(jsonVal User) string {
+	encode, _ := json.Marshal(jsonVal)
+	return base64.StdEncoding.EncodeToString(encode)
+}
+
+func undoJSON(cookie *http.Cookie) (User, bool) {
+	decode, _ := base64.StdEncoding.DecodeString(cookie.Value)
+	var jsonVal User
+	json.Unmarshal(decode, &jsonVal)
+	if hmac.Equal([]byte(jsonVal.Hmac), []byte(getHMAC(jsonVal.Uuid+jsonVal.Name+jsonVal.Age))) {
+		return jsonVal, true
+	}
+	return jsonVal, false
+}
+
+
+func serveLogin(res http.ResponseWriter, req *http.Request) {
 
 	cookie, err := req.Cookie("logged-in")
 
@@ -26,13 +74,15 @@ func login(res http.ResponseWriter, req *http.Request) {
 	// check log in
 	if req.Method == "POST" {
 		password := req.FormValue("password")
-		if password == "secret" {
+		if password == "pleb" {
 			cookie = &http.Cookie{
 				Name:  "logged-in",
 				Value: "1",
 				//Secure: true,
 				HttpOnly: true,
 			}
+
+
 		}
 	}
 
@@ -65,9 +115,6 @@ func login(res http.ResponseWriter, req *http.Request) {
 			<body>
 			<h1>LOG IN</h1>
 			<form method="POST">
-				<h3>Name</h3>
-				<input type="text" name="name">
-				<br>			
 				<h3>Password</h3>
 				<input type="text" name="password">
 				<br>
@@ -90,9 +137,38 @@ func login(res http.ResponseWriter, req *http.Request) {
 			<h1><a href="/logout">LOG OUT</a></h1>
 			</body>
 			</html>`
+		serveUserData(res,req)
+		bakeUserCookie(cookie, req)
 	}
 
 	io.WriteString(res, html)
 }
 
-//need to implement this to other file
+func serveUserData(res http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("session-fino")
+	if err != nil {
+		cookie = userCookie()
+		http.SetCookie(res, cookie)
+	}
+	if req.Method == "POST" {
+		cookie.Value = bakeUserCookie(cookie, req)
+	}
+	obj, _ := undoJSON(cookie)
+
+	t, _ := template.New("Name").Parse(loginFile)
+	t.Execute(res, obj)
+}
+
+
+func userCookie() *http.Cookie {
+	id, _ := uuid.NewV4()
+	temp := User{Uuid: id.String(), Hmac: getHMAC(id.String())}
+	b, _ := json.Marshal(temp)
+	encode := base64.StdEncoding.EncodeToString(b)
+	return &http.Cookie{
+		Name:     "session-fino",
+		Value:    encode,
+		HttpOnly: true,
+		//Secure : true,
+	}
+}
